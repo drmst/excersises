adcde05dd8f0e53a40c58502a24b82c14ffdcd9d
Mesut Ozturk <doktormesut@gmail.com>
2023-10-14 00:21:22 +0300

    suggestions were applied

--- quotemachine/app.js
+++ quotemachine/app.js
@@ -1,41 +1,39 @@
-
-
-
 const newQuoteButton = document.getElementById("new-quote");
 let quote = document.getElementById("quote-text");
-const randomColor = document.getElementsByClassName("random-color");
-const randomTextColor = document.querySelector("#quote-text");
+const elementThatChangeColor = document.getElementsByClassName("random-color");
+const quoteParagraph = document.querySelector("#quote-text");
 
-newQuoteButton.addEventListener("click", function() {
-  let randomColorTextforR = Math.floor(Math.random() * 256);
-  let randomColorTextforG = Math.floor(Math.random() * 256);
-  let randomColorTextforB = Math.floor(Math.random() * 256);
-  let randomRGB = "rgb(" + randomColorTextforR + "," + randomColorTextforG + "," + randomColorTextforB + ")";
+const quoteTexts = [
+  "Deadlines just aren't real to me until I'm staring one in the face.",
+  "A deadline is negative inspiration. Still, it's better than no inspiration at all.",
+  "I don't need time, I need a deadline.",
+  "Dreams don't have timelines, deadlines, and aren't always in straight lines.",
+  "Normally I miss deadlines like a storm trooper misses Jedi.",
+  "There's nothing an artist needs more - even more than excellent tools and stamina - than a deadline.",
+  "There’s no getting out alive, but you hope to avoid a deadline.",
+  "Respect your deadline like it’s a field marshal.",
+  "Deadline-induced quality issues: a problem all over the galaxy.",
+  "I do however like to tell people, especially regarding writing and deadlines: Don't be perfect, just be done.",
+  "Not for nothing are they called deadlines. By the time you reach the final line, sir, you are indeed dead.",
+  "A hammer made of deadlines is the surest tool for crushing writer's block.",
+  "One of these days, I won't have a painful deadline! That's a lie, I probably always will.",
+];
 
-  const quoteText = [
-    "Deadlines just aren't real to me until I'm staring one in the face.",
-    "A deadline is negative inspiration. Still, it's better than no inspiration at all.",
-    "I don't need time, I need a deadline.",
-    "Dreams don't have timelines, deadlines, and aren't always in straight lines.",
-    "Normally I miss deadlines like a storm trooper misses Jedi.",
-    "There's nothing an artist needs more - even more than excellent tools and stamina - than a deadline.",
-    "There’s no getting out alive, but you hope to avoid a deadline.",
-    "Respect your deadline like it’s a field marshal.",
-    "Deadline-induced quality issues: a problem all over the galaxy.",
-    "I do however like to tell people, especially regarding writing and deadlines: Don't be perfect, just be done.",
-    "Not for nothing are they called deadlines. By the time you reach the final line, sir, you are indeed dead.",
-    "A hammer made of deadlines is the surest tool for crushing writer's block.",
-    "One of these days, I won't have a painful deadline! That's a lie, I probably always will."
-  ];
+newQuoteButton.addEventListener("click", function () {
+  const randomRedColor = Math.floor(Math.random() * 256);
+  const randomGreenColor = Math.floor(Math.random() * 256);
+  const randomBlueColor = Math.floor(Math.random() * 256);
+  const randomRGB =
+    "rgb(" +randomRedColor + "," + randomGreenColor + "," + randomBlueColor + ")";
 
-  let randomQuoteTextNumber = Math.floor(Math.random() * quoteText.length);
-  quote.innerHTML = quoteText[randomQuoteTextNumber];
+  let randomQuoteTextNumber = Math.floor(Math.random() * quoteTexts.length);
+  quote.innerHTML = quoteTexts[randomQuoteTextNumber];
 
-  for (let i = 0; i < randomColor.length; i++) {
-    randomColor[i].style.backgroundColor = randomRGB;
+  for (let i = 0; i < elementThatChangeColor.length; i++) {
+    elementThatChangeColor[i].style.backgroundColor = randomRGB;
   }
 
-  randomTextColor.style.color = randomRGB;
+  quoteParagraph.style.color = randomRGB;
 
   document.body.appendChild(quote.innerHTML);
 });
